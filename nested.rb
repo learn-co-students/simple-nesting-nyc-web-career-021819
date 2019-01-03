f895ff0b1783ca1fef27071ff9b0bf92d91d3ca8
Khaled M. Khalifeh <github email address>
2019-01-03 16:42:40 +0200

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,15 +57,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+string_hash = programmer_hash[:dennis_ritchie][:languages]
+string_hash[0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,8 +77,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+new_hash = {:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP", "C"]}}
+new_hash.merge(programmer_hash)
+new_hash
 end
 
 def changing_alan
@@ -101,7 +100,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +121,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+languages = programmer_hash[:dennis_ritchie][:languages]
+languages << "Assembly"
+programmer_hash
 end
